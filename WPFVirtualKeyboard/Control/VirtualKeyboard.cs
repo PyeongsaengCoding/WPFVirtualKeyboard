3915bf95fb41c5f52a4ca18b99fa44a965a29077
dhddldid <dhddldid02@gmail.com>
2020-02-19 13:22:33 +0900

    Setting Hangul, IsEnableHook
    Hangul array content
    IsEnableHook = false

--- WPFVirtualKeyboard/Control/VirtualKeyboard.cs
+++ WPFVirtualKeyboard/Control/VirtualKeyboard.cs
@@ -364,7 +364,7 @@ namespace WPFVirtualKeyboard.Control
                 Hook.UseGlobal = UseGlobal;
                 Hook.Start();
             }
-
+            Send(System.Windows.Input.Key.HangulMode);
             Application.Current.MainWindow.Closed += (s, args) => Hook.Stop();
         }
 
@@ -384,11 +384,32 @@ namespace WPFVirtualKeyboard.Control
                     IsPressedCapsLock = Simulator.Input.InputDeviceState.IsTogglingKeyInEffect(VirtualKeyCode.CAPITAL);
                     UpdateHookData();
                 }
-
+                Send(System.Windows.Input.Key.HangulMode);
                 UpdateKeys();
             }
         }
 
         #endregion
+
+        public static void Send(System.Windows.Input.Key key)
+        {
+            if (Keyboard.PrimaryDevice != null)
+            {
+                if (Keyboard.PrimaryDevice.ActiveSource != null)
+                {
+                    var e = new KeyEventArgs(Keyboard.PrimaryDevice, Keyboard.PrimaryDevice.ActiveSource, 0, key)
+                    {
+                        RoutedEvent = Keyboard.KeyDownEvent
+                    };
+                    InputManager.Current.ProcessInput(e);
+
+                    // Note: Based on your requirements you may also need to fire events for:
+                    // RoutedEvent = Keyboard.PreviewKeyDownEvent
+                    // RoutedEvent = Keyboard.KeyUpEvent
+                    // RoutedEvent = Keyboard.PreviewKeyUpEvent
+                }
+            }
+        }
+
     }
 }
--- WPFVirtualKeyboard/MainWindow.xaml.cs
+++ WPFVirtualKeyboard/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+using WPFVirtualKeyboard.Helper;
 
 namespace WPFVirtualKeyboard
 {
@@ -23,11 +14,10 @@ namespace WPFVirtualKeyboard
         public MainWindow()
         {
             InitializeComponent();
-
-            //tbInput.AcceptsReturn = true;
-            //tbInput.Focus();
-
-            //generalKeyboard.IsEnableHook = true;
+            tbInput.Focus();            
         }
     }
+
+
+
 }
